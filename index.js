818ad64f974709b483a1aa5e2ae9f9c3912adb48
Aditya Jain <jn.ad1995@gmail.com>
2020-07-26 14:54:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ var todoSchema = new mongoose.Schema({
   item: String
 });
 
-// create a new model and base it on a Schema.
+/** create a new model and base it on a Schema.
+ * mongoose.model 1st args is the "Model" name in mongoDB and
+ * 2nd arg is schema on which model is based on.
+ */
 var Todo = mongoose.model('Todo', todoSchema);
 
 // create an instance of the Todo model
